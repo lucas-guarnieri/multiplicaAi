6dba8391a88262293c11b7e9a83bad7ef5d60bec
lucas-guarnieri <lucaslguarnieri@gmail.com>
2023-01-10 16:46:46 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        int i = 1;
+        System.out.println("Tabela de multiplicação de " + numero);
+        while ( i < 11) {
+            System.out.println(numero * i);
+            i++;
+        };
+    }
+}
